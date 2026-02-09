e8c3691f6b76f9dc3c88741a3a530f156bf5067e
FoxtrotEverchosen <filipbulanda00@gmail.com>
2026-02-09 18:32:08 +0100

    Improved stack implementation

--- stack.c
+++ stack.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 #include "stack.h"
 
-void init_stack(stack *s){
+int init_stack(stack *s){
     s->capacity = 32;
     s->size = 0;
     s->data = malloc(s->capacity * sizeof(int));
 
     if(s->data == NULL){
-        printf("Failed to allocate memory\n");
-        exit(1);
+        return 1;
     }
+
+    return 0;
 }
 
-void init_stack_with_capacity(stack *s, int capacity){
-    if(capacity < 0){
-        printf("Initialized stack cannot have capacity smaller than 0 \n");
-        exit(1);
+int init_stack_with_capacity(stack *s, int capacity){
+    if(capacity <= 0){
+        return 1;
     }
 
     s->capacity = capacity;
@@ -24,39 +24,40 @@ void init_stack_with_capacity(stack *s, int capacity){
     s->data = malloc(s->capacity * sizeof(int));
 
     if(s->data == NULL){
-        printf("Failed to allocate memory\n");
-        exit(1);
+        return 1;
     }
+
+    return 0;
 }
 
-void push(stack *s, int value){
+int push(stack *s, int value){
     if(s->size == s->capacity){
-        printf("Failed to push to stack due to no free space");
-        return;
+        return 1;
     }
 
     s->data[s->size++] = value;
+    return 0;
 }
 
-int pop(stack *s){
+int pop(stack *s, int *out){
     if(is_empty(s)){
-        printf("Cannot pop value from empty stack");
-        return;
+        return 1;
     }
 
-    return s->data[--s->size];
+    *out = s->data[--s->size];
+    return 0;
 }
 
-int peek(stack *s){
+int peek(stack *s, int *out){
     if(is_empty(s)){
-        printf("Cannot peek into empty stack");
-        return;
+        return 1;
     }
 
-    return s->data[s->size - 1];
+    *out = s->data[s->size - 1];
+    return 0;
 }
 
-int is_empty(stack *s){
+int is_empty(const stack *s){
     return s->size == 0;
 }
 
--- stack.h
+++ stack.h
@@ -7,12 +7,12 @@ typedef struct{
     int* data;
 }stack;
 
-void init_stack(stack *s);
-void init_stack_with_capacity(stack *s, int capacity);
+int init_stack(stack *s);
+int init_stack_with_capacity(stack *s, int capacity);
 void free_stack(stack *s);
-void push(stack *s, int value);
-int pop(stack *s);
-int peek(stack *s);
-int is_empty(stack *s);
+int push(stack *s, int value);
+int pop(stack *s, int *out);
+int peek(stack *s, int *out);
+int is_empty(const stack *s);
 
 #endif
