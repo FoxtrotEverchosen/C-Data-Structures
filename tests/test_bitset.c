35abcc91a9a2eca62f9abe446610daafae26f20f
FoxtrotEverchosen <filipbulanda00@gmail.com>
2026-02-12 14:11:50 +0100

    Add tests for popcount and bitset_union

--- tests/test_bitset.c
+++ tests/test_bitset.c
@@ -43,10 +43,73 @@ TEST(test_toggle_bit){
     drop_bitset(&b);
 }
 
+TEST(test_popcount) {
+    bitset b;
+    init_bitset(&b, 128);
+
+    ASSERT_EQ_SIZE_T( (size_t) 0, popcount(&b));
+    
+    for(int i = 0; i < 10; i++){
+        set_bit(&b, i*2);
+    }
+    
+    ASSERT_EQ_SIZE_T( (size_t) 10, popcount(&b));
+    
+    drop_bitset(&b);
+}
+
+TEST(test_union) {
+    bitset b1;
+    init_bitset(&b1, 128);
+
+    bitset b2;
+    init_bitset(&b2, 256);
+
+    for(int i = 0; i < 10; i++){
+        set_bit(&b1, i*2);
+    }
+
+    for(int i = 0; i < 10; i++){
+        set_bit(&b2, 255 - i);
+    }
+
+    bitset u_bistet = bitset_union(&b1, &b2);
+    ASSERT_EQ_SIZE_T( (size_t) 20, popcount(&u_bistet));
+
+    bitset b4;
+    init_bitset(&b4, 128);
+
+    bitset b5;
+    init_bitset(&b5, 256);
+
+    bitset u_bistet2 = bitset_union(&b4, &b5);
+    ASSERT_EQ_SIZE_T( (size_t) 0, popcount(&u_bistet2));
+
+    for(int i = 0; i < 10; i++){
+        set_bit(&b4, i*2);
+        set_bit(&b5, i*2);
+    }
+    
+    bitset u_bistet3 = bitset_union(&b4, &b5);
+    ASSERT_EQ_SIZE_T( (size_t) 10, popcount(&u_bistet3));
+
+    drop_bitset(&b1);
+    drop_bitset(&b2);
+    drop_bitset(&u_bistet);
+    drop_bitset(&b4);
+    drop_bitset(&b5);
+    drop_bitset(&u_bistet2);
+    drop_bitset(&u_bistet3);
+}
+
+
+
 int main(void) {
     RUN_TEST(test_set_bit);
     RUN_TEST(test_unset_bit);
     RUN_TEST(test_toggle_bit);
+    RUN_TEST(test_popcount);
+    RUN_TEST(test_union);
 
     return TEST_SUMMARY();
 }
