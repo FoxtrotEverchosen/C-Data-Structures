cd45456beb5f0f578e236c366dc4853c6d4248c8
FoxtrotEverchosen <filipbulanda00@gmail.com>
2026-02-12 13:44:29 +0100

    Add popcount and bitset_union functionalities for bitset

--- include/bitset.h
+++ include/bitset.h
@@ -7,6 +7,7 @@
 
 typedef struct{
     uint64_t * data;
+    size_t capacity;
     size_t bit_size;
 } bitset;
 
@@ -15,6 +16,8 @@ int set_bit(bitset *b, size_t idx);
 int unset_bit(bitset *b, size_t idx);
 int toggle_bit(bitset *b, size_t idx);
 int get_bit(const bitset *b, size_t idx);
+size_t popcount(bitset *b);
+bitset bitset_union(const bitset *b1, const bitset *b2);
 void drop_bitset(bitset *b);
 
 #endif
--- src/bitset.c
+++ src/bitset.c
@@ -1,3 +1,6 @@
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -10,7 +13,8 @@ int init_bitset(bitset *b, size_t size){
     b->bit_size = size;
     
     size_t long_count = (size + 63) / 64;
-    uint64_t *new_data = calloc(long_count, sizeof(uint64_t));
+    b->capacity = long_count;
+    uint64_t *new_data = calloc(b->capacity, sizeof(uint64_t));
     
     if(new_data == NULL){
         return 1;
@@ -44,9 +48,54 @@ int toggle_bit(bitset *b, size_t idx){
 }
 
 int get_bit(const bitset *b, size_t idx){
+    /*
+        This function returns -1 as an error value. Queried bit is returned as 0/1
+    */
     if (idx >= b->bit_size) return -1;
 
     return (b->data[idx >> 6] & (1ULL << (idx & 63))) != 0;
+}
+
+size_t popcount(bitset *b){
+    uint64_t sum = 0;
+
+    for(size_t i=0; i < b->capacity; i++){
+        sum += __builtin_popcountll(b->data[i]);
+    }
+    
+    return sum;
+}
+
+bitset bitset_union(const bitset *b1, const bitset *b2){
+    bitset new_bitset;
+    size_t size = max(b1->bit_size, b2->bit_size);
+
+    if (init_bitset(&new_bitset, size) != 0) {
+        new_bitset.data = NULL;
+        new_bitset.bit_size = 0;
+        new_bitset.capacity = 0;
+        return new_bitset;
+    }
+
+    if (b1->capacity == b2->capacity){    
+        for(size_t i = 0; i < b1->capacity; i++){
+            new_bitset.data[i] = b1->data[i] | b2->data[i];
+        }
+    }else{
+        size_t min_cap = min(b1->capacity, b2->capacity);
+
+        for(size_t i = 0; i < min_cap; i++){
+            new_bitset.data[i] = b1->data[i] | b2->data[i];
+        }
+
+        for (size_t i = min_cap; i < new_bitset.capacity; i++){
+            new_bitset.data[i] = b1->capacity > b2->capacity ? 
+                                    b1->data[i] : 
+                                    b2->data[i];
+        }
+    }
+
+    return new_bitset;
 }
 
 void drop_bitset(bitset *b){
